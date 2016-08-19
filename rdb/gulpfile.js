ccf15a7d27f12aa45dcb024f000a9301bf21b711
mamorum <mamoru310@yahoo.com>
2016-08-19 16:55:53 +0900

    Use maven, gulp and browser-sync for live reload.

--- rdb/gulpfile.js
+++ rdb/gulpfile.js
@@ -3,28 +3,32 @@ var
   child = require('child_process'),
   gulp = require('gulp'),
   gutil = require('gulp-util'),
-  livereload = require('gulp-livereload');
+  bs = require('browser-sync').create();
 
-
-// Linux にも対応する
+// TODO Linux にも対応する
 gulp.task('stop-app', function() {
   child.exec('taskkill /pid ' + p.pid + ' /f /t');
 });
-
 gulp.task('start-app', function() {
   p = child.spawn(
-    'C:\\gradle-2.10\\bin\\gradle.bat',
-    ['run'], ['']
+    'mvn.cmd', ['exec:java'], ['']
   );
   p.stdout.on('data', function(b) { process.stdout.write(b.toString()); });
   p.stderr.on('data', function(b) { process.stderr.write(b.toString()); });
   gutil.log("App PID:" + p.pid);
 });
 
-gulp.task('start-livereload', function() {
-  livereload.listen();  // port=35729
+gulp.task('bs-start', function() {
+  bs.init({
+    // BrowserSync waits on localhost:3000.
+    proxy: "localhost:8080",
+    open: true
+  });
 });
 
-gulp.task('default', ['start-app', 'start-livereload'], function() {
-  gulp.watch('src/**/*', ['stop-app', 'start-app']);
+gulp.task('default', ['start-app', 'bs-start'], function() {
+  // gulp.watch('src/**/*', ['stop-app', 'start-app']);
+
+  // Watch *.class compiled by IDE.
+  gulp.watch('target/classes/**/*', ['stop-app', 'start-app']);
 });
