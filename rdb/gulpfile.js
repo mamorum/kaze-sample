389e7f39af16cb866421a54d0396ac2251aead7e
mamorum <mamoru310@yahoo.com>
2016-08-20 15:50:19 +0900

    Update build.

--- rdb/gulpfile.js
+++ rdb/gulpfile.js
@@ -10,25 +10,29 @@ gulp.task('stop-app', function() {
   child.exec('taskkill /pid ' + p.pid + ' /f /t');
 });
 gulp.task('start-app', function() {
-  p = child.spawn(
-    'mvn.cmd', ['exec:java'], ['']
-  );
+  var vmarg =
+    '-Dbs-url=http://localhost:3000/__browser_sync__?method=reload';
+  p = child.spawn('mvn.cmd', ['exec:java', vmarg], ['']);
   p.stdout.on('data', function(b) { process.stdout.write(b.toString()); });
   p.stderr.on('data', function(b) { process.stderr.write(b.toString()); });
   gutil.log("App PID:" + p.pid);
 });
 
 gulp.task('bs-start', function() {
-  bs.init({
-    // BrowserSync waits on localhost:3000.
+  bs.init({ // listen on localhost:3000
     proxy: "localhost:8080",
-    open: true
+    open: false
   });
+});
+gulp.task('bs-reload', function() {
+  bs.reload();
 });
 
 gulp.task('default', ['start-app', 'bs-start'], function() {
-  // gulp.watch('src/**/*', ['stop-app', 'start-app']);
-
-  // Watch *.class compiled by IDE.
-  gulp.watch('target/classes/**/*', ['stop-app', 'start-app']);
+  // *.class compiled by IDE.
+  gulp.watch('target/classes/**/*.class', ['stop-app', 'start-app']);
+  // static contents copied by IDE.
+  gulp.watch('target/classes/public/**/*', ['bs-reload']);
+  // if compile is needed.
+  // gulp.watch('src/**/*', [...]);
 });
