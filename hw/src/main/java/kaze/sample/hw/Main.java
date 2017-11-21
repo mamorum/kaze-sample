2913e28a768bd675273adbe0e5a04a3cf66a8b31
mamorum <mamoru310@yahoo.com>
2017-11-21 11:13:57 +0900

    Update kaze veresion to 0.2.4-SNAPSHOT.

--- hw/src/main/java/kaze/sample/hw/Main.java
+++ hw/src/main/java/kaze/sample/hw/Main.java
@@ -1,13 +1,20 @@
 package kaze.sample.hw;
 
+import com.google.gson.Gson;
+
 import kaze.App;
 import kaze.server.Jetty;
 
 public class Main {
   public static void main(String[] args) {
-    App.get("/", (req, res) -> {
-      res.html("<p>Hello World</p>");
+    App app = new App("/app");
+    app.get("/hello", (req, res) -> {
+      res.json("msg", "Hello World.");
     });
+    Gson gson = new Gson();
+    app.parser(gson::fromJson, gson::toJson);
+    Jetty.app(app);
+    Jetty.doc("/public");
     Jetty.listen(8080);
   }
 }
