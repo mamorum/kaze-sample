e6ad7c059eb6046ebd362f8e38678c39e974202d
mamorum <mamorum@users.noreply.github.com>
2018-03-27 10:35:47 +0900

    Update kaze version.

---
+++ poml-all.bat
@@ -0,0 +1,28 @@
+call poml -v
+
+cd hw
+cd
+call poml
+cd ..
+
+cd jackson
+cd
+call poml
+cd ..
+
+cd rdb
+cd
+call poml
+cd ..
+
+cd tomcat
+cd
+call poml
+cd ..
+
+cd war
+cd
+call poml
+cd ..
+
+pause
