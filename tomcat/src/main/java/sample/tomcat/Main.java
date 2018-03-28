0744bfa9a3df89bec6174f7972333697372a9824
mamorum <mamorum@users.noreply.github.com>
2018-03-28 13:37:19 +0900

    Delete using kaze.

--- tomcat/src/main/java/sample/tomcat/Main.java
+++ tomcat/src/main/java/sample/tomcat/Main.java
@@ -1,4 +1,4 @@
-package kaze.sample.tomcat;
+package sample.tomcat;
 
 import java.io.File;
 
@@ -6,30 +6,19 @@ import org.apache.catalina.Context;
 import org.apache.catalina.servlets.DefaultServlet;
 import org.apache.catalina.startup.Tomcat;
 
-import kaze.App;
-
-// To check:
-//  app -> http://localhost:8080/app/hello
-//  doc -> http://localhost:8080/ (or /index.html)
 public class Main {
   public static void main(String[] args) throws Exception {
-    App app = new App();
-    app.get("/hello", (req, res) -> {
-      res.html("<p>Hello, Tomcat.</p>");
-    });
-    listen(app);
-  }
-  private static void listen(App app) throws Exception {
-    Tomcat cat = new Tomcat();  // embedded tomcat
+    Tomcat cat = new Tomcat();
     cat.setPort(8080);
     cat.setHostname("0.0.0.0");
+    File docDir = new File("src/main/resources/public");
     Context ctx = cat.addContext(
-      "", new File("src/main/resources/public").getAbsolutePath()
+      "", docDir.getAbsolutePath()
     );
     Tomcat.addServlet(ctx, "default", new DefaultServlet());
     ctx.addServletMappingDecoded("/", "default");
     ctx.addWelcomeFile("index.html");
-    Tomcat.addServlet(ctx, "app", app);
+    Tomcat.addServlet(ctx, "app", new HelloServlet());
     ctx.addServletMappingDecoded("/app/*", "app");
     cat.start();
     cat.getServer().await();
