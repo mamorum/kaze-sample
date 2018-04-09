c04c609e74dabc4c7db5ae1073cade076219ea08
mamorum <mamorum@users.noreply.github.com>
2018-04-09 16:01:00 +0900

    version up. add web.xml and jetty plugin. rename class.

--- war/src/main/java/kaze/sample/war/SyncApp.java
+++ war/src/main/java/kaze/sample/war/SyncApp.java
@@ -1,13 +1,11 @@
 package kaze.sample.war;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 
 import kaze.App;
 
-@WebServlet("/app/*")
 @SuppressWarnings("serial")
-public class SyncAppServlet extends App {
+public class SyncApp extends App {
   public void init() throws ServletException {
     get("/hello", (req, res) -> {
       res.write("text/plain", "Hello.");
