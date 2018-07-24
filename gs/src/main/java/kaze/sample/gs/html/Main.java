dd1ab023a6ebe9c7884ed17692c7bce4dcf52b3e
mamorum <mamorum310@outlook.jp>
2018-07-24 09:56:53 +0900

    Improve.

--- gs/src/main/java/kaze/sample/gs/html/Main.java
+++ gs/src/main/java/kaze/sample/gs/html/Main.java
@@ -3,12 +3,12 @@ package kaze.sample.gs.html;
 import kaze.App;
 import kaze.opt.Jetty;
 
-// app -> http://localhost:8080/app/html/hello
+// app -> http://localhost:8080/app/hello
 // doc -> http://localhost:8080/*
 public class Main {
   public static void main(String[] args) {
     App app = new App();
-    app.get("/html/hello", (req, res) -> {
+    app.get("/hello", (req, res) -> {
       res.html("<p>Hello, World.</p>");
     });
     Jetty.app(app, "/app/*");
